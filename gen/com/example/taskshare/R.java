7e48d2cca2b561ee79579d7c5c5fca2a922a1b7b
sangho3 <sangho3@ue19.cs.ualberta.ca>
2012-11-04 17:50:07 -0700

    strings.xml for FulfillPhotoTaskActivity. and FulfillPhotoTaskActivity.class updated

--- gen/com/example/taskshare/R.java
+++ gen/com/example/taskshare/R.java
@@ -52,9 +52,13 @@ public final class R {
         public static final int activity_view_task=0x7f060008;
     }
     public static final class string {
+        public static final int addPhotos1=0x7f040015;
+        public static final int addPhotos3=0x7f040016;
         public static final int app_name=0x7f040000;
+        public static final int description=0x7f040014;
         public static final int hello_world=0x7f040001;
         public static final int menu_settings=0x7f040002;
+        public static final int takePhoto=0x7f040018;
         public static final int text_description=0x7f04000c;
         public static final int text_fulfill_task=0x7f040011;
         public static final int text_name=0x7f04000b;
@@ -72,6 +76,7 @@ public final class R {
         public static final int title_activity_task_share=0x7f040008;
         public static final int title_activity_upload_photo=0x7f040006;
         public static final int title_activity_view_task=0x7f040009;
+        public static final int uploadPhoto=0x7f040017;
     }
     public static final class style {
         public static final int AppTheme=0x7f050000;
--- src/com/example/taskshare/FulfillPhotoTaskActivity.java
+++ src/com/example/taskshare/FulfillPhotoTaskActivity.java
@@ -1,20 +1,78 @@
 package com.example.taskshare;
 
+import java.io.File;
+
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
 import android.app.Activity;
+import android.content.Intent;
+import android.graphics.drawable.Drawable;
+
 import android.view.Menu;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.ImageView;
 
-public class FulfillPhotoTaskActivity extends Activity {
 
+public class FulfillPhotoTaskActivity extends Activity implements OnClickListener {
+	
+	
+	ImageView iv;
+	Uri outputFileUri;
+	Button takePhoto;
+	Button uploadPhoto;
+	Intent cameraIntent;
+	File file;
+	final static int cameraData = 0;
+	
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_fulfill_photo_task);
-    }
+        initialize();
+    	
 
-    @Override
+    }
+    private void initialize() {
+    	iv = (ImageView) findViewById(R.id.imageView1);
+    	takePhoto = (Button) findViewById(R.id.takePhoto);
+    	takePhoto.setOnClickListener(this);
+    	
+    }
+    
+ 
+	@Override
     public boolean onCreateOptionsMenu(Menu menu) {
         getMenuInflater().inflate(R.menu.activity_fulfill_photo_task, menu);
         return true;
     }
+	@Override
+	public void onClick(View v) {
+		// TODO Auto-generated method stub
+		File root = new File(Environment.getExternalStorageDirectory().getAbsolutePath() + "/tmp");
+		String folder = root.toString(); 
+	    file = new File(folder, "fileName" + ".jpg");
+	    Uri outputFileUri = Uri.fromFile(file);
+
+		cameraIntent = new Intent(android.provider.MediaStore.ACTION_IMAGE_CAPTURE);
+		cameraIntent.putExtra(MediaStore.EXTRA_OUTPUT, outputFileUri);
+		
+		startActivityForResult(cameraIntent, cameraData);
+	}
+
+	protected void onActivityResult(int requestCode, int resultCode, Intent data){
+		super.onActivityResult(requestCode, resultCode, data);
+		if(requestCode == RESULT_OK)
+		{
+			iv = (ImageView)findViewById(R.id.imageView1);
+			iv.setImageDrawable(Drawable.createFromPath(outputFileUri.getPath()));
+		
+		}
+	}
+
+		
 }
+	
